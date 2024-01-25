dedbe385aeb8b3a08d9074950bcff9a38dd89c41
MarceloSilveira <marcelosilverag@hotmail.com>
2024-01-25 14:20:40 -0300

    melhorando funcionalidade do Jquery

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
-const form = document.getElementById('form-tarefa');
-let linhas = '';
+$(document).ready(function(){                            // quando o documento pronto, chama a função
+    $('form').on('submit', function (e){                  // esta chamando o form e pegando o evento submit
+        e.preventDefault();                              // impede que o form recarregue
+        const nomeTarefa = $('input').val();             // const esta recebendo valor do campo input
+        constNovaTarefa = $(`<li>${nomeTarefa}</li>`)    // crase concatena texto e valor da variavel(pegando valor de NomeTarefa para a const NovaTarefa)
 
+        $(constNovaTarefa).appendTo('ul')                // passando o valor da NovaTarefa para a lista
+        $('input').val('')                               // string vazia para limpar o valor da input
+    })
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
+    $('ul').on('click', 'li', function(e){               // funçao chama "ul" e adiciona o evento click
+        e.preventDefault();
+        $(this).toggleClass('riscarTarefa')             //this informa o elemento que foi clicado, toggleclass implementa o riscarTarefa
 
-    const inputNomeTarefa = document.getElementById('nome-tarefa');
-    
-    linha = `<li>${inputNomeTarefa.value}</li>`;
-    linhas += linha;
-    
-    const corpoTabela = document.querySelector('li');
-    corpoTabela.innerHTML = linhas;
-    inputNomeTarefa.value = '';
+    })
 
-
-    corpoTabela.addEventListener('click', function (e) {
-    e.target.classList.add('checked');
-    }) 
 })
-
-    
-
